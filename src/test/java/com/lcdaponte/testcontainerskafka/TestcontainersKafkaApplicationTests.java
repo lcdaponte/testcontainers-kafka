0de0aaa2f396229af0fb6d7cd7c29e8fd2021dec
Lucas Cavalcante <lcdaponte@gmail.com>
2022-03-29 15:44:09 -0300

    Initial commit

---
+++ src/test/java/com/lcdaponte/testcontainerskafka/TestcontainersKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.testcontainerskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
